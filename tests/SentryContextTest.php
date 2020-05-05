e709b21052dedf6afd23f1993561428286bdb448
zingimmick <zingimmick@outlook.com>
2020-05-05 19:16:59 +0800

    fix style.

--- tests/SentryContextTest.php
+++ tests/SentryContextTest.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Sentry\Event;
 use Sentry\Laravel\Facade;
-use Sentry\SentrySdk;
 use Zing\LaravelSentry\Middleware\SentryContext;
 
 class SentryContextTest extends TestCase
