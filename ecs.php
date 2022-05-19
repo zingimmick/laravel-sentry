4025b6eeec0f2525a5e36e4ba9b62a2f5942e6bf
Zing <zingimmick@outlook.com>
2022-05-19 20:40:06 +0800

    Use ESCConfig instead of parameters

--- ecs.php
+++ ecs.php
@@ -9,14 +9,12 @@ use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_72, ECSSetList::CUSTOM]);
-    $parameters = $ecsConfig->parameters();
     $ecsConfig->parallel();
     $ecsConfig->skip([
         // bug
         ReturnAssignmentFixer::class,
     ]);
-    $parameters->set(
-        Option::PATHS,
+    $ecsConfig->paths(
         [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
     );
 };
