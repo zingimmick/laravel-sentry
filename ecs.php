79bcbd3de1bbbf72f1a2c154ab94a1d2328f2ace
Zing <zingimmick@outlook.com>
2022-05-19 20:42:13 +0800

    Use ESCConfig instead of parameters (#103)

--- ecs.php
+++ ecs.php
@@ -4,19 +4,14 @@ declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_72, ECSSetList::CUSTOM]);
-    $parameters = $ecsConfig->parameters();
     $ecsConfig->parallel();
     $ecsConfig->skip([
         // bug
         ReturnAssignmentFixer::class,
     ]);
-    $parameters->set(
-        Option::PATHS,
-        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
-    );
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']);
 };
